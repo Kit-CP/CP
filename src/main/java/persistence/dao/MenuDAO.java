c9776b06a9e2acfd63b71483bc63310ad1af9ca9
daehyun <kimdeahyeon5445@gmail.com>
2022-11-16 15:46:28 +0900

    6

--- src/main/java/persistence/dao/MenuDAO.java
+++ src/main/java/persistence/dao/MenuDAO.java
@@ -4,8 +4,10 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import persistence.dto.MenuDTO;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class MenuDAO {
     private final SqlSessionFactory sqlSessionFactory;
@@ -40,5 +42,31 @@ public class MenuDAO {
             sqlSession.close();
         }
         return result;
+    }
+
+    public void updateMenu(String name, String newname, int price) {
+        SqlSession sqlSession = sqlSessionFactory.openSession(false);
+        Map<String, String> param = new HashMap<>();
+        param.put("menu_name", name);
+        param.put("new_menu_name", newname);
+        param.put("new_price", String.valueOf(price));
+        try {
+            sqlSession.update("mapper.MenuMapper.updateMenu", param);
+            sqlSession.commit();
+        }
+        catch (Exception e) {
+            sqlSession.rollback();
+        }
+        finally {
+            sqlSession.close();
+        }
+    }
+
+    public void updateMenu(String name) {
+
+    }
+
+    public void updateMenu(int price) {
+
     }
 }
