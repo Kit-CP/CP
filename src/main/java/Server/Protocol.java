a66c03e59723fa737192fcff563d437f421c24b3
박찬진 <qarkchanjin@gmail.com>
2022-12-02 20:56:55 +0900

    중간 지점 저장

---
+++ src/main/java/Server/Protocol.java
@@ -0,0 +1,94 @@
+package Server;
+
+import org.apache.log4j.xml.Log4jEntityResolver;
+
+import javax.xml.crypto.Data;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+public class Protocol {
+
+    public final static int HEADER_SIZE = 4;
+    //TYPE
+    public final static byte SINE_UP = 1;
+    public final static byte LOGIN = 2;
+    public final static byte REGISTER = 3;
+    public final static byte ACCEPT = 4;
+    public final static byte CORRECTION = 5;
+    public final static byte INQUIRY = 6;
+
+    //CODE
+    //TYPE:1
+    public final static byte REGISTER_INFO = 1;
+    //TYPE:2
+    public final static byte LOGIN_INFO = 1;
+    //TYPE:3 , CLIENT
+    public final static byte ORDER = 1;
+    public final static byte REVIEW = 2;
+
+    //TYPE:3 , OWNER
+    public final static byte MENU_INSERT = 1;
+    public final static byte STORE_INSERT = 2;
+    public final static byte OPTION_INSERT = 3;
+
+    //TYPE:4 , CLIENT
+    public final static byte CANCEL_ORDER = 1;
+
+    //TYPE:4, OWNER
+    public final static byte ACCEPT_ORDER = 1;
+
+    //TYPE:4 , MANAGER
+    public final static byte ACCEPT_MENU = 1;
+
+    //TYPE:5 , CLIENT
+    public final static byte CHANGE_CLIENT_INFO = 1;
+
+    //Type:5 , OWNER
+    public final static byte CHANGE_OWNER_INFO = 1;
+    public final static byte CHANGE_STORE_INFO = 2;
+
+    //TYPE:5 , MANAGER
+    public final static byte CHANGE_MANAGER_INFO = 1;
+
+    //TYPE:6 , CLIENT
+    public final static byte ORDER_LIST = 1;
+    public final static byte STORE_LIST = 2;
+    public final static byte MENU_LIST = 3;
+
+    //TYPE:6 , OWNER
+    public final static byte MYSTORE_LIST = 1;
+    public final static byte MYMENU_LIST = 2;
+    public final static byte MYORDER_LIST = 3;
+    public final static byte MYTOTAL_LIST = 3;
+
+    //TYPE:7 , MANAGER
+    public final static byte ALL_STORE_LIST = 1;
+    public final static byte ALL_MENU_LIST = 2;
+    public final static byte INFO_LIST = 3;
+    public final static byte TOTAL_LIST = 4;
+
+    //AUTHORITY
+    public final static byte ANONYMITY= 0;
+    public final static byte CLIENT = 1;
+    public final static byte OWNER = 2;
+    public final static  byte MANAGER = 3;
+
+    //ANSWER
+    public final static byte DEFAULT = 0;
+    public final static byte SUCCESS = 1;
+    public final static byte ERROR = 2;
+
+    public static byte[] getHeader(byte type, byte code, byte authority, byte answer, int size) throws  IOException {
+        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(bao);
+
+        dos.writeByte(type);
+        dos.writeByte(code);
+        dos.writeByte(authority);
+        dos.writeByte(answer);
+        dos.writeInt(size);
+
+        return bao.toByteArray();
+    }
+}
---
+++ src/main/java/Server/ServerTCP.java
@@ -0,0 +1,66 @@
+package Server;
+
+import Database.persistence.dto.*;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class ServerTCP {
+    public static void main(String[] args) {
+        ServerSocket ss = null;
+        Socket socket = null;
+        DataInputStream dis;
+        DataOutputStream dos;
+        ObjectInputStream ois; //구현한 객체 역직렬화.
+        ObjectOutputStream oos; // 구현한 객체 직렬화.
+
+        try {
+            ss = new ServerSocket(7777);
+            System.out.println("create Server. Waiting Connection...\n\n");
+
+            socket = ss.accept();
+
+            System.out.println("User Connected!!\n");
+
+            dis = new DataInputStream(socket.getInputStream());
+
+            dos = new DataOutputStream(socket.getOutputStream());
+
+
+            while (true) {
+                //헤더 정보 읽어오기.
+                byte type = dis.readByte();
+                byte code = dis.readByte();
+                byte authority = dis.readByte();
+                byte answer = dis.readByte();
+                int size = dis.readInt();
+
+                byte[] body = null;
+                DataInputStream bodyInfo;
+
+                if (size > 0) { //body 읽어오기
+                    body = new byte[size];
+                    dis.read(body);
+                    bodyInfo = new DataInputStream(new ByteArrayInputStream(body));
+                    ois = new ObjectInputStream(bodyInfo); // 직접 구현한 직렬화, 역직렬화를 사용.
+
+                    ois.readObject(); // 객체 읽기 >> 각 케이스에 따른 객체 선언 및 연결
+
+                } else { //body가 없는 경우. >> 헤더 정보만 있을 경우.
+
+                }
+            }
+        }catch(IOException e) {
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+            System.out.println(e);
+        }finally {//무조건 적으로 마지막 수행.
+            if(socket != null) {
+                try{
+                    socket.close(); // 나중에 생각하기.
+                }catch(IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
