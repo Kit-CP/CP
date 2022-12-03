112c8b667098d97452651a7214f1600b0e299422
daehyun03 <kimdeahyeon5445@gmail.com>
2022-12-03 21:30:55 +0900

    StoreDTO 역/직렬화 코드추가 storemapper 수정

--- src/main/java/Database/persistence/dto/StoreDTO.java
+++ src/main/java/Database/persistence/dto/StoreDTO.java
@@ -4,10 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 @Getter
 @Setter
 @ToString
-public class StoreDTO {
+public class StoreDTO implements IDTO {
     private String store_name;
     private String store_address;
     private String store_phone;
@@ -15,5 +20,32 @@ public class StoreDTO {
     private String information;
     private int isAccept;
     private String user_ID;
-    private UserDTO userDTO;
+
+    public byte[] getBytes() throws IOException {
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(buf);
+
+        dos.writeUTF(store_name);
+        dos.writeUTF(store_address);
+        dos.writeUTF(store_phone);
+        dos.writeInt(store_score);
+        dos.writeUTF(information);
+        dos.writeInt(isAccept);
+        dos.writeUTF(user_ID);
+
+        return buf.toByteArray();
+    }
+
+    public static StoreDTO readStoreDTO(DataInputStream dis) throws IOException {
+        StoreDTO dto = new StoreDTO();
+        dto.setStore_name(dis.readUTF());
+        dto.setStore_address(dis.readUTF());
+        dto.setStore_phone(dis.readUTF());
+        dto.setStore_score(dis.readInt());
+        dto.setInformation(dis.readUTF());
+        dto.setIsAccept(dis.readInt());
+        dto.setUser_ID(dis.readUTF());
+
+        return dto;
+    }
 }
