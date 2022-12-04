c38db90c4e15445cf3d6483b638e56ef6ff5b5f0
daehyun03 <kimdeahyeon5445@gmail.com>
2022-12-05 02:11:01 +0900

    리스트 역/직렬화 됨 근데 너무 길면 짤리는데 이게 필요한가?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,31 @@ import Database.persistence.MyBatisConnectionFactory;
 import Database.persistence.dao.OrderDAO;
 import Database.persistence.dao.UserDAO;
 import Database.persistence.dto.*;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
+        List<MenuSalesDTO> list = new ArrayList<>();
+        list.add(new MenuSalesDTO("name", 1, 1));
+        list.add(new MenuSalesDTO("name1", 1, 1));
+        list.add(new MenuSalesDTO("name2", 1, 1));
+        byte[] bytes = new byte[0];
+        for ( MenuSalesDTO dto : list ) {
+                bytes = ArrayUtils.addAll(bytes, dto.getBytes());
+        }
+
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        DataInputStream dis = new DataInputStream(bis);
+
+        for ( int i = 0; i < 3; i++ ) {
+            MenuSalesDTO dto = MenuSalesDTO.readMenuSalesDTO(dis);
+            System.out.println(dto.toString());
+        }
     }
 }
