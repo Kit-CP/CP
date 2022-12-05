81f36cff00f93b98d55777097813350e54cef6ee
hodu42 <knn330@naver.com>
2022-12-06 02:55:11 +0900

    printALL 수정

--- src/main/java/Database/view/UserView.java
+++ src/main/java/Database/view/UserView.java
@@ -7,9 +7,8 @@ import java.util.List;
 
 public class UserView {
     public static void printAll(List<UserDTO> dtos) {
-        System.out.printf("%-7s %-3s %-10s %-4s %-15s %-3s %-3s\n", "유저 ID", "권한", "주소", "이름", "전화번호", "나이", "상태");
         for (UserDTO dto : dtos) {
-            System.out.printf("%-7s %-3s %-10s %-4s %-15s %-3s %-3s\n", dto.getUser_ID(), State.getAuthority(dto.getAuthority()), dto.getUser_address(), dto.getUser_name(), dto.getUser_phone(), dto.getAge(), State.getState(dto.getState()));
+            System.out.printf("ID : %s, 권한 : %s, 주소 : %s, 이름 : %s, 전화번호 : %s, 나이 : %d, 상태 : %s\n", dto.getUser_ID(), State.getAuthority(dto.getAuthority()), dto.getUser_address(), dto.getUser_name(), dto.getUser_phone(), dto.getAge(), State.getState(dto.getState()));
         }
     }
 }
