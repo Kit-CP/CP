60ab63477cb0f6d70f10052cb7f54c81959e4bcf
박찬진 <qarkchanjin@gmail.com>
2022-12-07 02:15:45 +0900

    보류 중인 메뉴 출력 수정

--- src/main/java/Database/view/MenuView.java
+++ src/main/java/Database/view/MenuView.java
@@ -10,16 +10,13 @@ public class MenuView {
     public static void printPendingMenus(List<MenuDTO> dtos) {
         for (MenuDTO dto : dtos) {
             StringBuilder sb = new StringBuilder();
+            sb.append("가게 이름: ");
+            sb.append(dto.getStore_name());
+            sb.append("메뉴 이름: ");
             sb.append(dto.getMenu_name());
-            sb.append(", ");
-            sb.append(dto.getStore_name());
-            sb.append(", ");
-            sb.append(dto.getCategory());
-            sb.append(", ");
+            sb.append("메뉴 가격: ");
             sb.append(dto.getMenu_price());
-            sb.append(", ");
-            sb.append(dto.getStore_name());
-            sb.append(", ");
+            sb.append("현재 상태: ");
             sb.append(State.getState(dto.getState()));
             System.out.println(sb);
         }
