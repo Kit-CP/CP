1f80d00158643e84b846c03cb270eaedec9446c3
daehyun03 <kimdeahyeon5445@gmail.com>
2022-12-07 02:08:54 +0900

    리뷰뷰 수정 옵션등록시 옵션 보이게 수정

--- src/main/java/Database/view/ReviewView.java
+++ src/main/java/Database/view/ReviewView.java
@@ -6,21 +6,9 @@ import java.util.List;
 
 public class ReviewView {
     public static void printAll(List<ReviewDTO> list, int crtPage , int numOfPages) {
-        System.out.println("리뷰번호\t아이디\t\t별점\t\t\t\t\t주문번호\t\t주문내용\t\t답글");
         for (ReviewDTO dto : list) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(dto.getReview_id());
-            sb.append("\t\t");
-            sb.append(dto.getUser_id());
-            sb.append("\t\t");
-            sb.append(dto.getReview_score());
-            sb.append("\t\t");
-            sb.append(dto.getOrder_id());
-            sb.append("\t\t\t");
-            sb.append(dto.getContent());
-            sb.append("\t\t\t");
-            sb.append(dto.getReply());
-            System.out.println(sb);
+            System.out.printf("리뷰번호 : %d  |  아이디 : %s  |  별점 : %d  |  주문번호 : %d  |  리뷰내용 : %s  |  답글 : %s\n",
+                    dto.getReview_id(), dto.getUser_id(), dto.getReview_score(), dto.getOrder_id(), dto.getContent(), dto.getReply());
         }
         System.out.printf("현재 페이지 번호 : %d\n", crtPage);
         System.out.printf("마지막 페이지 번호 : %d\n", numOfPages/2 + numOfPages%2);
